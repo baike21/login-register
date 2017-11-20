18e8282e59d209c24af54818797c19114921b36f
baike21 <baike21@sina.com>
2017-11-20 16:23:44 +0800

    third commit:Add email format authenticate function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,44 +1,58 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+﻿import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 
-import { AlertService, AuthenticationService } from '../_services/index';
+import {AlertService, AuthenticationService} from '../_services/index';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+  moduleId: module.id,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent implements OnInit {
-    model: any = {};
-    loading = false;  // 是否加载中的属性,同submitted
-    returnUrl: string;
+  model: any = {};
+  loading = false;  // 是否加载中的属性,同submitted
+  returnUrl: string;
+  validEmail = false;  // email格式检查
 
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService,
-        private alertService: AlertService) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private authenticationService: AuthenticationService,
+              private alertService: AlertService) {
+  }
 
-    ngOnInit() {
-        // reset login status
-        this.authenticationService.logout();
+  ngOnInit() {
+    // reset login status
+    this.authenticationService.logout();
 
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // 邮箱格式校验
+  email_format_authenticate(raw_email: string) {
+    console.log('输入的邮箱是'+raw_email);
+    const emailRegex = /^[A-Za-z0-9]+([-_.][A-Za-z0-9]+)*@([A-Za-z0-9]+[-.])+[A-Za-z0-9]{2,5}$/;
+    if (emailRegex.test(raw_email)) {
+      this.validEmail = true;
+    } else {
+      this.validEmail = false;
     }
+    console.log('当前状态validEmail'+this.validEmail);
 
-    login() {
-        this.loading = true;
-        // 验证
-        this.authenticationService.login(this.model.username, this.model.password)
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+
+  }
+
+  login() {
+    this.loading = true;
+    this.authenticationService.login(this.model.username, this.model.password)
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+  }
 }
