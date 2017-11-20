3a7ed407b0e214518751f00c500b983484d414e8
baike21 <baike21@sina.com>
2017-11-20 12:00:48 +0800

    first commit

---
+++ src/app/_models/user.ts
@@ -0,0 +1,5 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AlertService, AuthenticationService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+    model: any = {};
+    loading = false;  // 是否加载中的属性,同submitted
+    returnUrl: string;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService) { }
+
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    login() {
+        this.loading = true;
+        // 验证
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+}
