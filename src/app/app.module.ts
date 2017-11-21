44b4974b4ed8f1a2f5c29daf531da71ac98d1203
baike21 <baike21@sina.com>
2017-11-21 19:40:07 +0800

    fifth commit: package username input form-control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-﻿import { NgModule }      from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 // used to create fake backend
@@ -8,8 +8,8 @@ import { fakeBackendProvider } from './_helpers/index';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { BaseRequestOptions } from '@angular/http';
 
-import { AppComponent }  from './app.component';
-import { routing }        from './app.routing';
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 
 import { AlertComponent } from './_directives/index';
 import { AuthGuard } from './_guards/index';
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,30 @@
-﻿import {Component, OnInit} from '@angular/core';
+﻿import {Component, Directive, OnInit} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
-
+import {User} from '../_models/user';
 import {AlertService, AuthenticationService} from '../_services/index';
+import {isUndefined} from "util";
+
 
 @Component({
-  moduleId: module.id,
+  selector: 'app-login-container',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent implements OnInit {
-  model: any = {};
-  loading = false;  // 是否加载中的属性,同submitted
-  returnUrl: string;
-  validEmail = true;  // email格式检查
-  usernameIsfocus = true;
-  passwdIsfocus = false;
-  loginformSubmitted = false;
+  user = new User();
+
+  public loading = false;  // 正在去服务器申请登陆中的状态
+  public submitted = false;  // 用户已经按下登陆键时为真
+  public validemail = true;  // email格式检查
+
+  public usernamefocus = true;  // 光标是否聚焦
+  public usernameblur = false;
+  public passwdfocus = false;
+  public passwdblur = false;
+
+  returnUrl: string;  // 成功登陆后返回的URL
+  currentStyles: {};  // 当前输入框的样式
 
   constructor(private route: ActivatedRoute,
               private router: Router,
@@ -24,7 +32,16 @@ export class LoginComponent implements OnInit {
               private alertService: AlertService) {
   }
 
+  // username控件的样式设置
+  setCurrentStyles() {
+    this.currentStyles = {
+      'box-shadow': this.usernamefocus ? '0 0 10px #4169E1' : this.invalidUsername() ? '0 0 10px #B22222' : 'none',
+    };
+    return this.currentStyles;
+  }
+
   ngOnInit() {
+    this.setCurrentStyles();
     // reset login status
     this.authenticationService.logout();
 
@@ -32,43 +49,88 @@ export class LoginComponent implements OnInit {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  // 邮箱格式校验
-  email_format_authenticate(raw_email: string) {
-    // this.usernameIsfocus = false;  // 光标移走后开验证
-    // console.log('输入的邮箱是'+raw_email);
+  // blur时进行邮箱格式校验
+  checkEmail(raw_email: string) {
+    this.usernamefocus = false;  // 光标移走后开验证
+    this.usernameblur = true;  // 光标移走了
+    // console.log('输入的邮箱是' + raw_email);
     const emailRegex = /^[A-Za-z0-9]+([-_.][A-Za-z0-9]+)*@([A-Za-z0-9]+[-.])+[A-Za-z0-9]{2,5}$/;
     if (emailRegex.test(raw_email)) {
-      this.validEmail = true;
+      this.validemail = true;  // 格式正确
+      this.setCurrentStyles();
     } else {
-      this.validEmail = false;
+      this.validemail = false;
+      this.setCurrentStyles();
     }
-    // console.log('当前状态validEmail'+this.validEmail);
+    // console.log('当前状态email格式判断' + this.validemail);
   }
 
-  // 光标聚焦到用户名输入框后关验证
+  // 查看状态，判读是否显示 气泡 邮箱格式有误
+  invalidEmailFormat() {
+    // console.log('当前值' + this.user.username);
+    if ( !( isUndefined(this.user.username) || this.user.username === '' ) && !this.validemail && this.usernameblur && !(/\s+/.test(this.user.username))) {
+      return true;  // 不为空且格式不对且失去焦点且不是无效字符时  气泡显示
+    } else {
+      return false;  // 为空或者格式正确或者获得焦点时气泡消失
+    }
+  }
+
+  // 光标聚焦到username输入框的事件
   usernameFocus() {
-    this.usernameIsfocus = true;
-    this.passwdIsfocus = false;
-    this.loginformSubmitted = false;
+    this.usernamefocus = true;
+    this.usernameblur = false;
+    this.passwdfocus = false;
+    this.submitted = false;
+    this.setCurrentStyles();
   }
 
-  // 光标聚焦到用户名输入框后关验证
+  // 光标聚焦到password输入框的事件
   passwdFocus() {
-    this.passwdIsfocus = true;
-    this.usernameIsfocus = false;
-    this.loginformSubmitted = false;
+    this.passwdfocus = true;
+    this.usernamefocus = false;
+    this.usernameblur = true;
+    this.submitted = false;
+    this.setCurrentStyles();
   }
 
-  changeloginformStatus() {
-    this.loginformSubmitted = true;
-    this.passwdIsfocus = false;
-    this.usernameIsfocus = false;
+  // 查看状态,决定提交后是否显示空用户名的气泡,并更新当前输入框样式
+  invalidSubmitUsername() {
+    if (this.submitted && !this.usernamefocus) {
+      if (isUndefined(this.user.username) || this.user.username === '' || /\s+/.test(this.user.username)) {
+        return true;  // 不合法的用户名输入,显示错误提示给用户
+      } else {
+        return false;
+      }
+    } else {
+      return false;  //  用户没点提交键，不显示
+    }
   }
 
-  login() {
-    if (this.validEmail) {
+  // 查看状态，不合法的用户名返回真（包括邮箱格式不正确，空格缩进换行分页等无效字符）
+  invalidUsername() {
+    if (this.invalidEmailFormat() || this.invalidSubmitUsername()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  //
+  //
+
+
+  // 登陆键被点击
+  submit(user) {
+    this.submitted = true;
+    this.passwdfocus = false;
+    this.passwdblur = true;
+    this.usernamefocus = false;
+    this.usernameblur = true;
+
+    if (!this.invalidUsername()) {
       this.loading = true;
-      this.authenticationService.login(this.model.username, this.model.password)
+      this.authenticationService.login(this.user.username, this.user.password)
         .subscribe(
           data => {
             this.router.navigate([this.returnUrl]);
@@ -77,7 +139,7 @@ export class LoginComponent implements OnInit {
             this.alertService.error(error);
             this.loading = false;
           });
-    }else {
+    } else {
       return false;
     }
   }
